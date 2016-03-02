9e96c28e07790c1f5a144978e08143bcadd9a897
Jessica Fix and Ryan Brown <jcfix.pdx@gmail.com>
2016-03-02 15:55:23 -0800

    Add getName, Id for new Patron class

---
+++ src/Patron.php
@@ -0,0 +1,82 @@
+<?php
+	class Patron
+	{
+		private $name;
+		private $id;
+
+		function __construct($name, $id)
+		{
+			$this->name = $name;
+			$this->id = $id;
+		}
+
+		function getName()
+        {
+            return $this->name;
+        }
+
+		function getId()
+        {
+            return $this->id;
+        }
+
+		// function save()
+		// {
+		// 	$GLOBALS['DB']->exec("INSERT INTO authors (name) VALUES ('{$this->getName()}');");
+		// 	$this->id = $GLOBALS['DB']->lastInsertId();
+		// }
+		//
+		// static function getAll()
+		// {
+		// 	$query = $GLOBALS['DB']->query("SELECT * FROM authors;");
+		// 	$authors = array();
+		// 	foreach($query as $author) {
+		// 		$name = $author['name'];
+		// 		$id = $author['id'];
+		// 		$new_author = new Patron($name, $id);
+		// 		array_push($authors, $new_author);
+		// 	}
+		// 	return $authors;
+		// }
+		//
+		// static function deleteAll()
+		// {
+		// 	$GLOBALS['DB']->exec("DELETE FROM authors;");
+		// }
+		//
+		// function addBook($book)
+		// {
+		// 	$GLOBALS['DB']->exec("INSERT INTO books_authors (book_id, author_id) VALUES ({$book->getId()}, {$this->getId()});");
+		// }
+		//
+		// function getBooks()
+		// {
+		// 	$query = $GLOBALS['DB']->query("SELECT books.* FROM authors
+		// 		JOIN books_authors ON (authors.id = books_authors.author_id)
+		// 		JOIN books ON (books_authors.book_id = books.id)
+		// 		WHERE authors.id = {$this->getId()};");
+		// 	$book_ids = $query->fetchAll(PDO::FETCH_ASSOC);
+		//
+		// 	$books = array();
+		// 	foreach($book_ids as $book) {
+		// 		$title = $book['title'];
+		// 		$id = $book['id'];
+		// 		$new_book = new Book($title, $id);
+		// 		array_push($books, $new_book);
+		// 	}
+		// 	return $books;
+		// }
+		//
+		// static function findByPatron($search_name)
+		// {
+		// 	$found_author = null;
+		// 	$authors = Patron::getAll();
+		// 	foreach($authors as $author) {
+		// 		if ($search_name == $author->getName()) {
+		// 			$found_author = $author;
+		// 		}
+		// 	}
+		// 	return $found_author;
+		// }
+	}
+ ?>
---
+++ tests/PatronTest.php
@@ -0,0 +1,156 @@
+<?php
+	/**
+	* @backupGlobals disabled
+	* @backupStaticAttributes disabled
+	*/
+
+	require_once "src/Author.php";
+	require_once "src/Book.php";
+	require_once "src/Copy.php";
+	require_once "src/Patron.php";
+
+	$server = 'mysql:host=localhost;dbname=library_test';
+	$user = 'root';
+	$password = 'root';
+	$DB = new PDO($server, $user, $password);
+
+	class PatronTest extends PHPUnit_Framework_TestCase
+	{
+		// protected function tearDown()
+        // {
+        //     Patron::deleteAll();
+        //     Book::deleteAll();
+        // }
+
+		function testGetName()
+        {
+            //Arrange
+            $name = "Jessica Fix";
+            $id = 1;
+            $test_patron = new Patron($name, $id);
+
+            //Act
+            $result = $test_patron->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+		function testGetId()
+        {
+            //Arrange
+			$name = "Jessica Fix";
+            $id = 1;
+            $test_patron = new Patron($name, $id);
+
+            //Act
+            $result = $test_patron->getId();
+
+			//Assert
+			$this->assertEquals(1, $result);
+		}
+		//
+		// function testSave()
+		// {
+		// 	//Arrange
+		// 	$name = "Junot Diaz";
+        //     $id = 1;
+        //     $test_author = new Patron($name, $id);
+		//
+		// 	//Act
+		// 	$test_author->save();
+		//
+		// 	//Assert
+		// 	$result = Patron::getAll();
+		// 	$this->assertEquals($test_author, $result[0]);
+		// }
+		//
+		// function testGetAll()
+		// {
+		// 	//Arrange
+		// 	$name = "Junot Diaz";
+        //     $id = 1;
+        //     $test_author = new Patron($name, $id);
+		// 	$test_author->save();
+		//
+		// 	$name2 = "Gabriel Garcia Marquez";
+		// 	$id2 = 2;
+		// 	$test_author2 = new Patron($name2, $id2);
+		// 	$test_author2->save();
+		//
+		// 	//Act
+		// 	$result = Patron::getAll();
+		//
+		// 	//Assert
+		// 	$this->assertEquals([$test_author, $test_author2], $result);
+		// }
+		//
+		// function testAddBook()
+		// {
+		// 	//Arrange
+		// 	$title = "The General in his Labyrinth";
+		// 	$id = 1;
+		// 	$test_book = new Book($title, $id);
+        //     $test_book->save();
+		//
+		// 	$name = "Gabriel Garcia Marquez";
+        //     $id = 1;
+        //     $test_author = new Patron($name, $id);
+		// 	$test_author->save();
+		//
+		// 	//Act
+		// 	$test_author->addBook($test_book);
+		//
+		// 	//Assert
+		// 	$this->assertEquals($test_author->getBooks(), [$test_book]);
+		// }
+		//
+		// function testGetBooks()
+        // {
+        //     //Arrange
+		// 	$name = "Gabriel Garcia Marquez";
+        //     $id = 1;
+        //     $test_author = new Patron($name, $id);
+		// 	$test_author->save();
+		//
+		// 	$title = "The General in his Labyrinth";
+		// 	$id = 1;
+		// 	$test_book = new Book($title, $id);
+        //     $test_book->save();
+		//
+		// 	$title2 = "No One Writes to the Colonel";
+		// 	$id2 = 2;
+		// 	$test_book2 = new Book($title2, $id2);
+        //     $test_book2->save();
+		//
+        //     //Act
+        //     $test_author->addBook($test_book);
+        //     $test_author->addBook($test_book2);
+		//
+        //     //Assert
+        //     $this->assertEquals($test_author->getBooks(), [$test_book, $test_book2]);
+        // }
+		//
+		// function testFindByPatron()
+        // {
+        //     //Arrange
+		// 	$name = "Gabriel Garcia Marquez";
+        //     $id = 1;
+        //     $test_author = new Patron($name, $id);
+		// 	$test_author->save();
+		//
+		// 	$title = "The General in his Labyrinth";
+		// 	$id = 1;
+		// 	$test_book = new Book($title, $id);
+        //     $test_book->save();
+		//
+        //     //Act
+        //     $result = Patron::findByPatron($test_author->getName());
+		//
+        //     //Assert
+        //     $this->assertEquals($test_author, $result);
+        // }
+
+	}
+
+?>
