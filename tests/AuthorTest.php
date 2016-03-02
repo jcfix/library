8552012b2ff013159ff8554b8eb6bc9c848244d4
Jessica Fix and Ryan Brown <browneryan87@gmail.com>
2016-03-02 10:26:26 -0800

    Add passing getName and getId to Author.php

--- tests/AuthorTest.php
+++ tests/AuthorTest.php
@@ -1,22 +1,54 @@
 <?php
+	/**
+	* @backupGlobals disabled
+	* @backupStaticAttributes disabled
+	*/
 
-	require_once 'src/Author.php';
+	require_once "src/Author.php";
+	// require_once "src/Book.php";
+
+	$server = 'mysql:host=localhost;dbsubject=library_test';
+	$usersubject = 'root';
+	$password = 'root';
+	$DB = new PDO($server, $usersubject, $password);
 
 	class AuthorTest extends PHPUnit_Framework_TestCase
 	{
-	//
-	// 	// function test_makeTitleCase_oneWord()
-	// 	// {
-	// 	// //Arrange
-	// 	// $test_TitleCaseGenerator = new TitleCaseGenerator;
-	// 	// $input = 'beowulf';
-	// 	//
-	// 	// //Act
-	// 	// $result = $test_TitleCaseGenerator->makeTitleCase($input);
-	// 	//
-	// 	// //Assert
-	// 	// $this->assertEquals('Beowulf', $result);
-	// 	// }
+		// protected function tearDown()
+        // {
+        //     Author::deleteAll();
+        //     // Course::deleteAll();
+        // }
+
+		function testGetName()
+        {
+            //Arrange
+            $name = "Harry Houdini";
+            $id = 1;
+            $test_author = new Author($name, $id);
+
+            //Act
+            $result = $test_author->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+		function testGetId()
+        {
+            //Arrange
+            $name = "Harry Houdini";
+            $id = 1;
+            $test_author = new Author($name, $id);
+
+            //Act
+            $result = $test_author->getId();
+
+			//Assert
+			$this->assertEquals(1, $result);
+		}
+
+
 	}
 
 ?>
