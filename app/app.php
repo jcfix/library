f434b6790fe8a29442af94f8a3e0395637ecfecf
Jessica Fix and Ryan Brown <jcfix.pdx@gmail.com>
2016-03-03 09:27:56 -0800

    Add route/form to delete specific book

--- app/app.php
+++ app/app.php
@@ -18,20 +18,35 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
-
+	// Gets homepage
 	$app->get('/', function() use ($app) {
 		return $app['twig']->render('index.html.twig');
 	});
 
+	// Gets librarian page with book-list and add-book form
 	$app->get('/librarian', function() use ($app) {
 		return $app['twig']->render('librarian.html.twig');
 	});
 
+	// Posts a book to librarian page
 	$app->post('/add_book', function() use ($app) {
 		$new_book = new Book($_POST['title']);
 		$new_book->save();
 		return $app['twig']->render('librarian.html.twig', array('books' => Book::getAll()
 	  ));
+	});
+
+	// Gets specific book page with add-author form
+	$app->get('/book/{id}', function($id) use ($app) {
+		$book = Book::find($id);
+		return $app['twig']->render('book.html.twig', array('book' => $book, 'authors' => $book->getAuthors()));
+	});
+
+	// Removes a specific book from library
+	$app->delete('/book/{id}/delete', function($id) use ($app) {
+		$book = Book::find($id);
+		$book->deleteBook();
+		return $app['twig']->render('librarian.html.twig', array('books' => Book::getAll()));
 	});
 
 
